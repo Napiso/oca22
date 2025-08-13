6b6920fe616913358abc102fefe54fddd78e4df6
Fabrício Diniz <dinizinhoprofaker@gmail.com>
2025-08-13 10:54:00 -0300

    fix: remover import duplicado no _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,5 @@
-import import "../styles/globals.css";
-export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
